744b1bf062259495e3182339165964851fa12d02
israelmark <israelmark98@gmail.com>
2024-11-07 23:45:06 +0200

    fix(): remove as string from .env vars

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import "./index.css";
 import { FronteggProvider } from "@frontegg/react";
 
 const contextOptions = {
-  baseUrl: import.meta.env.VITE_FRONTEGG_BASE_URL as string,
-  clientId: import.meta.env.VITE_FRONTEGG_CLIENT_ID as string,
+  baseUrl: import.meta.env.VITE_FRONTEGG_BASE_URL || "",
+  clientId: import.meta.env.VITE_FRONTEGG_CLIENT_ID || "",
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
